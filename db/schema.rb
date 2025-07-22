25596d728102b987106fe9cc52fa8f2992b65795
名前 <>
2025-07-22 11:12:33 +0900

    jun

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_22_020716) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "isbn"
+    t.string "category"
+    t.integer "total_copies"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
